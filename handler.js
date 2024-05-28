c2f0f3cbb9c48e17088260b82d03e3ce99026d5b
WillyRumapea <willyrumapea123jr@gmail.com>
2024-05-28 12:52:50 +0700

    connection with database mysql

--- handler.js
+++ handler.js
@@ -1,18 +1,33 @@
-const jamur = require("./jamur");
+const pool = require("./connection");
 
-const getAllMold = () => {
-  return {
-    status: "success",
-    data: {
-      jamur,
-    },
-  };
+const getAllMold = async () => {
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    const [rows] = await connection.execute("SELECT * FROM table_jamur");
+    return {
+      status: "success",
+      data: {
+        table_jamur: rows,
+      },
+    };
+  } catch (err) {
+    console.error(err);
+    return h
+      .response({
+        status: "fail",
+        message: "Database query failed",
+      })
+      .code(500);
+  } finally {
+    if (connection) connection.release();
+  }
 };
 
 const getMoldById = (request, h) => {
   const { id } = request.params;
 
-  const mold = jamur.find((n) => n.id === id);
+  const mold = jamur.filter((n) => n.id === id)[0];
 
   if (mold) {
     return {
@@ -41,7 +56,9 @@ const changeMold = (request, h) => {
     if (dampak) mold.dampak = dampak;
 
     if (tambahkanData) {
-      Object.assign(mold, tambahkanData);
+      Object.keys(tambahkanData).forEach((key) => {
+        mold[key] = tambahkanData[key];
+      });
     }
 
     return h.response({
